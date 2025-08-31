4067484b4aefe55fa28a28c4189eff40dd0e20eb
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-08-31 13:16:33 -0600

    Agregar filterquestions.js: implementar función para generar respuestas automáticas basadas en la entrada del usuario

---
+++ class_3/utils/filterquestions.js
@@ -0,0 +1,27 @@
+const buildCannedResponse = (userInput) => {
+  const input = userInput.toLowerCase();
+
+  if (
+    input.includes("clima") ||
+    input.includes("frio") ||
+    input.includes("calor")
+  ) {
+    return "Para el clima, te recomiendo ropa ligera si hace calor y unca chamarra deportiva si hace frío";
+  }
+
+  if (input.includes("talla")) {
+    return "Nuestras tallas van de XS a XL, ¿Me puedes decir tu estatura y peso aproximado para recomendarte mejor";
+  }
+
+  if (
+    input.includes("gimnasio") ||
+    input.includes("correr") ||
+    input.includes("yoga")
+  ) {
+    return "Para el gimnasio: shorts transpirables y camiseta dry-fit. Para correr: tenis adecuads y ropa ligera. Para yoga: ropa cómoda y flexible";
+  }
+
+  return null;
+};
+
+module.exports = { buildCannedResponse };
