c60173604059aca27e116ffc9b3f0174cadc96f3
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-09-04 15:27:11 -0600

    Refactor configuración de middleware: simplificar uso de CORS y agregar morgan para registro de peticiones

--- class_4/backEnd/chatbot.js
+++ class_4/backEnd/chatbot.js
@@ -4,16 +4,14 @@ require("dotenv").config();
 const { chatIA } = require("./controllers/chat_controller");
 const { PORT } = process.env;
 const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express(); // Crear instancia para express
 connectDB(); //Llamamos a la funcion de connectDB para que funcione la base de datos
 
-app.use(
-  cors({
-    origin: "http://127.0.0.1:59902",
-  })
-);
+app.use(cors({}));
 app.use(express.json()); // Middleware para leer el cuerpo de las peticiones en json
+app.use(morgan("dev")); // Middleware para ver las peticiones en consola
 
 app.post("/ask", chatIA);
 
