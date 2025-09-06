e16623b4e8dea881b7b28e784a14b76a463a2544
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-09-05 18:02:09 -0600

    Agregar archivo main.js: implementar la lógica del chatbot para manejar mensajes y respuestas

---
+++ class_final/frontEnd/main.js
@@ -0,0 +1,62 @@
+const elements = {
+  chatContainer: document.getElementById("messages"),
+  form: document.getElementById("chatForm"),
+  userInput: document.getElementById("userInput"),
+};
+
+localStorage.removeItem("user_id");
+localStorage.setItem("user_id", "23344556"); //Prueba de user_id
+const apiUri = "http://localhost:8080/ask";
+
+const appendMessage = (message, sender) => {
+  const messageElem = document.createElement("div");
+  messageElem.classList.add("message", sender);
+
+  messageElem.textContent = message;
+  console.log("Appending message: ", message);
+
+  elements.chatContainer.appendChild(messageElem);
+  elements.chatContainer.scrollTop = elements.chatContainer.scrollHeight;
+  if (message.includes("Error") || message.includes("Network error")) {
+    messageElem.style.color = "red";
+  }
+};
+
+const sendMessage = async (message) => {
+  const userId = localStorage.getItem("user_id");
+  try {
+    appendMessage("Escribiendo...", "bot");
+    const response = await fetch(apiUri, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ question: message, user_id: userId }),
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      elements.chatContainer.lastChild.remove(); //Remover "Escribiendo..."
+      appendMessage(data.respuesta, "bot");
+    } else {
+      elements.chatContainer.lastChild.remove();
+      appendMessage("Error:" + data.message, "bot");
+    }
+  } catch (err) {
+    setTimeout(() => {
+      elements.chatContainer.lastChild.remove();
+      appendMessage("Network error!", "bot");
+    }, 2000);
+  }
+};
+
+elements.form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userMessage = elements.userInput.value.trim();
+  if (!userMessage) return;
+
+  appendMessage(userMessage, "user");
+  elements.userInput.value = "";
+
+  sendMessage(userMessage);
+});
